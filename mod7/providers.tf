45d4591b4b550c0ac3ea580ba512fa49d97afa4d
Knutibordal <knut.i.bordal@gmail.com>
2024-10-07 22:02:39 +0200

    Miljø-ressurser

---
+++ mod7/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rgbackendkib001"
+    storage_account_name = "sabackendtfskibn66lrtrn"
+    container_name       = "scbackendtfskib"
+    key                  = "rg_web_mod7.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
